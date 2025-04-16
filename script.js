2719cd81869629fb0463b8c7e0d2b6814b094d0e
Jharsh01 <152757820+Jharsh01@users.noreply.github.com>
2025-04-15 20:04:56 -0400

    added javascipt and css

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const faders = document.querySelectorAll('.fade-in');
+  
+    const appearOnScroll = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    }, {
+      threshold: 0.1,
+    });
+  
+    faders.forEach(fader => appearOnScroll.observe(fader));
+  });
+  
